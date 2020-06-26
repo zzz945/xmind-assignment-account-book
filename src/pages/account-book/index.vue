519339d37e20dd563e7006b0c7a344003599efad
zhangdaiyan <zhangdaiyan@didiglobal.com>
2020-06-26 22:41:04 +0800

    feat: create a simple project structure

---
+++ src/pages/account-book/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="account-book">
+    Account Book
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccountBook',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
